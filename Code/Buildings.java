e57c4dea2a82fd56659501c2a2a42cfd0ebb2c82
jpquade <jpquade@gmail.com>
2016-04-10 18:47:33 -0700

    Added files via upload

--- Code/Buildings.java
+++ Code/Buildings.java
@@ -20,28 +20,28 @@ public class Buildings {
 	//setters
 	public boolean buildHouse(int stone, int wood)
 	{
-		if(stone <= 5 && wood <= 5)
+		if(stone >= 5 && wood >= 5)
 			house = true;
 		return house;
 	}
 	
 	public boolean buildFence(int stone, int wood)
 	{
-		if(stone <= 2 && wood <= 5)
+		if(stone >= 2 && wood >= 5)
 			fence = true;
 		return house;
 	}
 	
 	public boolean buildWell(int stone, int wood)
 	{
-		if(stone <= 5 && wood <= 2)
+		if(stone >= 5 && wood >= 2)
 			well = true;
 		return well;
 	}
 	
 	public boolean buildMill(int stone, int wood)
 	{
-		if(stone <= 5 && wood <= 0)
+		if(stone >= 5)
 			mill = true;
 		return mill;
 		
@@ -49,7 +49,7 @@ public class Buildings {
 	
 	public boolean buildMine(int stone, int wood)
 	{
-		if(stone == 0 && wood == 5)
+		if(wood >= 5)
 			mine = true;
 		return mine;
 	}
