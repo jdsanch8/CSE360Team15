4f1f4bf36097499a322193eed6c03e13b4eeb0aa
jpquade <jpquade@gmail.com>
2016-04-11 12:08:34 -0700

    Added files via upload

--- Code/Buildings.java
+++ Code/Buildings.java
@@ -8,89 +8,106 @@ public class Buildings {
 	private boolean mine;
 	private boolean farm;
 	
+	private boolean houseBuilt;
+	private boolean fenceBuilt;
+	private boolean wellBuilt;
+	private boolean millBuilt;
+	private boolean mineBuilt;
+	
+	
 	//constructor
-	public Buildings()
-	{
+	public Buildings(){
 		house = false;
 		fence = false;
 		well = false;
 		mill = false;
 		mine = false;
 		farm = false;
+		
+		houseBuilt = false;
+		fenceBuilt = false;
+		wellBuilt = false;
+		millBuilt = false;
+		mineBuilt = false;
 	}
 	
 	//setters
-	public boolean buildHouse(int stone, int wood)
-	{
-		if(stone >= 5 && wood >= 5)
-			house = true;
+	public boolean buildHouse(int stone, int wood){
+		if(!houseBuilt){
+			if(stone >= 5 && wood >= 5){
+				house = true;
+			}
+		}
 		return house;
 	}
 	
-	public boolean buildFence(int stone, int wood)
-	{
-		if(stone >= 2 && wood >= 5)
-			fence = true;
+	public boolean buildFence(int stone, int wood){
+		if(!fenceBuilt){
+			if(stone >= 2 && wood >= 5){
+				fence = true;
+			}
+		}
 		return house;
 	}
 	
-	public boolean buildWell(int stone, int wood)
-	{
-		if(stone >= 5 && wood >= 2)
-			well = true;
+	public boolean buildWell(int stone, int wood){
+		if(!wellBuilt){
+			if(stone >= 5 && wood >= 2){
+				well = true;
+			}
+		}
 		return well;
 	}
 	
-	public boolean buildMill(int stone, int wood)
-	{
-		if(stone >= 5)
-			mill = true;
-		return mill;
-		
+	public boolean buildMill(int stone, int wood){
+		if(!millBuilt){
+			if(stone >= 5){
+				mill = true;
+			}
+		}
+			return mill;		
 	}
 	
-	public boolean buildMine(int stone, int wood)
-	{
-		if(wood >= 5)
-			mine = true;
+	public boolean buildMine(int stone, int wood){
+		if(!mineBuilt){
+			if(wood >= 5){
+				mine = true;
+			}
+		}
 		return mine;
 	}
 	
-	public boolean buildfarm(int stone, int wood, int food)
-	{
-		if(stone >= 3 && wood >= 3 && food >= 3)
-			farm = true;
+	public boolean buildfarm(int stone, int wood, int food){
+		if(houseBuilt && fenceBuilt && wellBuilt){
+			if(stone >= 3 && wood >= 3 && food >= 3){
+				farm = true;
+			}
+		}
 		return farm;
 	}
 	
 	//getters
-	public boolean getHouse()
-	{
+	public boolean getHouse(){
 		return house;
 	}
 	
-	public boolean getFence()
-	{
+	public boolean getFence(){
 		return fence;
 	}
 	
-	public boolean getWell()
-	{
+	public boolean getWell(){
 		return well;
 	}
 	
-	public boolean getMill()
-	{
+	public boolean getMill(){
 		return mill;
 	}
 	
-	public boolean getMine()
-	{
+	public boolean getMine(){
 		return mine;
 	}
 	
-	public boolean getFarm()
-	{
+	public boolean getFarm(){
 		return farm;
 	}
 	
