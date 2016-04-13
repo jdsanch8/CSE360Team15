51c4b4990a1e0e217e1c10698a4655968aa92969
Laz9587 <Lazaro_9587@hotmail.com>
2016-04-13 14:44:36 -0700

    Added files via upload

--- Code/GameDriver.java
+++ Code/GameDriver.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class GameDriver {
 
 	public static void main(String[] args) {
-		Game myGame = new Game("Lazaro");
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter your name: ");
+		String playerName = in.next();
+		Game myGame = new Game(playerName);
 		myGame.gameController();
 	}
 
