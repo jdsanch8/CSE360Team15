b4fb222ff69d218becb4cde71f936f267cb44bf5
jdsanch8 <ijames495@yahoo.com>
2016-04-13 13:10:23 -0700

    Added files via upload
    Added update, and non-negative

--- Code/Dice.java
+++ Code/Dice.java
@@ -2,6 +2,7 @@
  * Generators random results, and modifies the values in the game object accordingly
  */
 
+import java.lang.*;
 import java.util.Random;
 
 public class Dice{
@@ -13,7 +14,7 @@ public class Dice{
 	boolean badLuck;
 	boolean house;
 	boolean fence;
-	
+
 	/**
 	 * Constructor, sets initial values to false, and multiplier to 6
 	 */
@@ -32,11 +33,11 @@ public class Dice{
 	public int rollMultiplier(){ //Returns Multiplier
 		int tempValue = 0;
 		if(goodLuck){
-			tempValue = (roller.nextInt() % multipler + 1) * 2;
+			tempValue = (Math.abs(roller.nextInt()) % multipler + 1) * 2;
 			goodLuck = false;
 		}
 		else
-			tempValue = roller.nextInt() % multipler + 1;
+			tempValue = Math.abs(roller.nextInt()) % multipler + 1;
 		if(badLuck){
 			if(house)
 				multipler = 8;
@@ -53,10 +54,10 @@ public class Dice{
 	 * @param config game containing the resource information
 	 */
 	public void rollWood(int mult, Game config){ //Access game class
-		int value = roller.nextInt() % DICE_SIZE + 1;
+		int value = Math.abs(roller.nextInt(6)) % DICE_SIZE + 1;
 		if(value <= 4){
 			System.out.println("The mill has produced " + mult + " wood!");
-			config.setWood(mult);
+			config.updateWood(mult);
 		}
 	}
 
@@ -66,9 +67,9 @@ public class Dice{
 	 * @param config game containing the resource information
 	 */
 	public void rollStone(int mult, Game config){ //Access game class
-		int value = roller.nextInt() % DICE_SIZE + 1;
+		int value = Math.abs(roller.nextInt()) % DICE_SIZE + 1;
 			if(value <= 4){
-				config.setStone(mult);
+				config.updateStone(mult);
 				System.out.println("The mine has produced " + mult + " stone!");
 			}
 	}
@@ -79,14 +80,14 @@ public class Dice{
 	 * @param config game containing the resource information
 	 */
 	public void rollBase(int mult, Game config){ //Access Game Class
-		int value = roller.nextInt() % DICE_SIZE + 1;
+		int value = Math.abs(roller.nextInt()) % DICE_SIZE + 1;
 		switch (value){
 		case 1:
-			config.setWood(mult);
+			config.updateWood(mult);
 			System.out.println("The workers have produced " + mult + " wood!");
 			break;
 		case 2:
-			config.setStone(mult);
+			config.updateStone(mult);
 			System.out.println("The workers have produced " + mult + " stone!");
 			break;
 		case 3:
@@ -103,7 +104,7 @@ public class Dice{
 		}
 
 	}
-	
+
 	/**
 	 * If despiar is rolled on base die, this triggers something bad to happen
 	 * @param mult value rolled from multipler die
@@ -112,25 +113,25 @@ public class Dice{
 	private void rollDespair(int mult, Game config){ //Access Game Class
 		if(fence)
 			mult = mult/2;
-		int value = roller.nextInt() % DICE_SIZE + 1;
+		int value = Math.abs(roller.nextInt()) % DICE_SIZE + 1;
 				switch (value){
 				case 1:
 					System.out.println("The thief has stolen " + mult + " wood!");
-					config.setWood(-mult);
+					config.updateWood(-mult);
 					break;
 				case 2:
 					System.out.println("The thief has stolen " + mult + " stone!");
-					config.setStone(-mult);
+					config.updateStone(-mult);
 					break;
 				case 3:
 					System.out.println("The thief has stolen " + mult + " food!");
-					config.setFood(-mult);
+					config.updateFood(-mult);
 					break;
 				case 4:
 					System.out.println("The thief has stolen " + mult + " of everything!");
-					config.setWood(-mult);
-					config.setStone(-mult);
-					config.setFood(-mult);
+					config.updateWood(-mult);
+					config.updateStone(-mult);
+					config.updateFood(-mult);
 					break;
 				case 5:
 				case 6:
@@ -140,14 +141,14 @@ public class Dice{
 					break;
 				}
 	}
-	
+
 	/**
 	 * If skill is rolled on base die, this triggers something good to happen
 	 * @param mult value rolled from multipler die
 	 * @param config game containing the resource information
 	 */
 	private void rollSkill(int mult, Game config){ //Access Game Class
-		int value = roller.nextInt() % DICE_SIZE + 1;
+		int value = Math.abs(roller.nextInt()) % DICE_SIZE + 1;
 		switch (value){
 		case 1:
 			System.out.println("Fate favors you, and the gods have increased current wood by 50%");
@@ -158,13 +159,13 @@ public class Dice{
 			config.setStone((int)(config.getStone()*1.5));
 			break;
 		case 3:
-			config.setFood(mult);
+			config.updateFood(mult);
 			break;
 		case 4:
 			System.out.println("Fate favors you, and the gods have increased everything by " + mult);
-			config.setWood(mult);
-			config.setStone(mult);
-			config.setFood(mult);
+			config.updateWood(mult);
+			config.updateStone(mult);
+			config.updateFood(mult);
 			break;
 		case 5:
 		case 6:
