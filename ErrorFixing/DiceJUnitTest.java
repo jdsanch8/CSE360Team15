1d02d670d1e871c7f032cb90cb90e97eadcf624d
jpquade <jpquade@gmail.com>
2016-04-28 09:21:55 -0700

    Added files via upload

--- ErrorFixing/DiceJUnitTest.java
+++ ErrorFixing/DiceJUnitTest.java
@@ -1,12 +1,15 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-/*
- * Temporary File - Update in progress
- */
 
 public class DiceJUnitTest {
 
+	@Test
+	public void diceCreate(){		//Ensures that dice object is created
+		Dice roller = new Dice();
+		assertNotNull(roller);
+	}
+	
 	@Test
 	public void diceRoller(){		//Ensures value are within range
 		Dice roller = new Dice();
@@ -21,7 +24,7 @@ public class DiceJUnitTest {
 		int testNum = roller.rollMultiplier();
 		assertTrue(testNum >= 1 && testNum <= 8);
 	}
-	
+/*	
 	@Test
 	public void diceModifyWood(){ //Ensures methods Dice uses to change wood works
 		Game driver = new Game("Team15");
@@ -30,6 +33,27 @@ public class DiceJUnitTest {
 		assertEquals(15, driver.getWood());
 	}
 	
+	@Test
+	public void diceRollWood(){ //Ensures method rollWood returns a string from the inputs
+		Game driver = new Game("Team15");
+		Dice die = new Dice();
+		assertEquals("The mill has produced 3 wood!", die.rollWood(3, driver));
+	}
+	
+	@Test
+	public void diceRollStone(){ //Ensures method rollStone returns a string from the inputs
+		Game driver = new Game("Team15");
+		Dice die = new Dice();
+		assertEquals("The mine has produced 2 stone!", die.rollStone(2, driver));
+	}
+	
+	@Test
+	public void diceBase(){ //Ensures methods rollBase case 1 functions correctly
+		Game driver = new Game("Team15");
+		Dice die = new Dice();
+		assertEquals("The workers have produced 1 wood!", die.rollBase(1, driver));
+	}
+*/	
 	@Test
 	public void diceModifyFood(){ //Ensures methods Dice uses to change food works
 		Game driver = new Game("Team15");
---
+++ ErrorFixing/GameJUnitTest.java
@@ -0,0 +1,280 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameJUnitTest {
+
+	@Test
+	public void objectCreation() { //Ensures Game object is created
+		Game driver = new Game("Team15");
+		assertNotNull(driver);	
+	}
+	
+	@Test
+	public void nameTest() { //Ensures name is read in properly
+		Game driver = new Game("Team15");
+		assertEquals("Team15", driver.getName());	
+	}
+	
+	@Test
+	public void rollMultReturn() { //Ensures a roll multiplier is being returned
+		Game driver = new Game("Team15");
+		assertNotNull(driver.rollMult());	
+	}
+	
+	@Test
+	public void rollMillReturn() { //Ensures a mill roll is being returned
+		Game driver = new Game("Team15");
+		assertNotNull(driver.rollMill(5));	
+	}
+	
+	@Test
+	public void rollMineReturn() { //Ensures a mine roll is being returned
+		Game driver = new Game("Team15");
+		assertNotNull(driver.rollMine(5));	
+	}
+	
+	@Test
+	public void rollEventReturn() { //Ensures an event roll is being returned
+		Game driver = new Game("Team15");
+		assertNotNull(driver.rollEvent(5));	
+	}
+	
+	@Test
+	public void startDay(){ //Ensures days start at 0
+		Game driver = new Game("Team15");
+		assertEquals(0, driver.getDays());
+	}
+	
+	@Test
+	public void testBuildMineCost(){ //Ensures mine takes resources correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		driver.buildMine();
+		assertTrue(driver.getStone() == 15 && driver.getWood() == 10);
+	}
+	
+	@Test
+	public void testBuildMine(){ //Ensures mine is built correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		assertTrue(driver.buildMine());
+	}
+	
+	@Test
+	public void testBuildMillCost(){ //Ensures mill takes resources correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		driver.buildMill();
+		assertTrue(driver.getStone() == 10 && driver.getWood() == 15);
+	}
+	
+	@Test
+	public void testBuildMill(){ //Ensures mill is built correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		assertTrue(driver.buildMill());
+	}
+	
+	@Test
+	public void testBuildHouseCost(){ //Ensures house takes resources correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		driver.buildHouse();
+		assertTrue(driver.getStone() == 10 && driver.getWood() == 10);
+	}
+	
+	@Test
+	public void testBuildHouse(){ //Ensures house is built correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		assertTrue(driver.buildHouse());
+	}
+	
+	@Test
+	public void testBuildFenceCost(){ //Ensures fence takes resources correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		driver.buildFence();
+		assertTrue(driver.getStone() == 13 && driver.getWood() == 10);
+	}
+	
+	@Test
+	public void testBuildFence(){ //Ensures fence is built correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		assertTrue(driver.buildFence());
+	}
+	
+	@Test
+	public void testBuildWellCost(){ //Ensures well takes resources correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		driver.buildWell();
+		assertTrue(driver.getStone() == 10 && driver.getWood() == 13);
+	}
+	
+	@Test
+	public void testBuildWell(){ //Ensures well is built correctly
+		Game driver = new Game("Team15");
+		driver.setStone(15);
+		driver.setWood(15);
+		assertTrue(driver.buildWell());
+	}
+	
+	@Test
+	public void testRequiredBuildingsNotBuilt(){ //Ensures that the unbuilt buildings are being returned
+		Game driver = new Game("Team15");
+		assertEquals("Well Fence House ", driver.requiredBuildingsStatus());
+	}
+	
+	@Test
+	public void testRequiredBuildingsBuilt(){ //Ensures that the built buildings are being returned as blank
+		Game driver = new Game("Team15");
+		driver.setStone(50);
+		driver.setWood(50);
+		driver.buildHouse();
+		driver.buildFence();
+		driver.buildWell();
+		assertEquals("", driver.requiredBuildingsStatus());
+	}
+	
+	@Test
+	public void testBuildFarmCost(){ //Ensures farm takes resources correctly
+		Game driver = new Game("Team15");
+		
+		driver.setStone(50);
+		driver.setWood(50);
+		driver.buildHouse();
+		driver.buildFence();
+		driver.buildWell();
+		driver.setStone(5);
+		driver.setWood(5);
+		driver.setFood(5);
+		driver.buildFarm();
+		assertTrue(driver.getStone() == 2 && driver.getWood() == 2 && driver.getFood() == 2);
+	}
+	
+	@Test
+	public void testBuildFarm(){ //Ensures farm is built correctly
+		Game driver = new Game("Team15");
+		
+		driver.setStone(50);
+		driver.setWood(50);
+		driver.buildHouse();
+		driver.buildFence();
+		driver.buildWell();
+		driver.setStone(5);
+		driver.setWood(5);
+		driver.setFood(5);
+		assertTrue(driver.buildFarm());
+	}
+	
+	@Test
+	public void testSetDays(){ //Ensures setDays works correctly
+		Game driver = new Game("Team15");
+		driver.setDays(2);
+		assertEquals(2, driver.getDays());
+	}
+	
+	@Test
+	public void testSetFood(){ //Ensures setFood works correctly
+		Game driver = new Game("Team15");
+		driver.setFood(2);
+		assertEquals(2, driver.getFood());
+	}
+	
+	@Test
+	public void testSetBuildings(){ //Ensures setBuildings works correctly
+		Game driver = new Game("Team15");
+		driver.setBuildings(1);
+		assertEquals(1, driver.getBuildings());
+	}
+	
+	@Test
+	public void testSetWood(){ //Ensures setWood works correctly
+		Game driver = new Game("Team15");
+		driver.setWood(3);
+		assertEquals(3, driver.getWood());
+	}
+	
+	@Test
+	public void testSetStone(){ //Ensures setStone works correctly
+		Game driver = new Game("Team15");
+		driver.setStone(1);
+		assertEquals(1, driver.getStone());
+	}
+	
+	@Test
+	public void testGetEnvironment(){ //Ensures getEnvironment works correctly
+		Game driver = new Game("Team15");
+		assertEquals(0, driver.getEnvironment());
+	}
+	
+	@Test
+	public void testGetDays(){ //Ensures getDays works correctly
+		Game driver = new Game("Team15");
+		assertEquals(0, driver.getDays());
+	}
+	
+	@Test
+	public void testGetFood(){ //Ensures getFood works correctly
+		Game driver = new Game("Team15");
+		assertEquals(45, driver.getFood());
+	}
+	
+	@Test
+	public void testGetBuildings(){ //Ensures getBuildings works correctly
+		Game driver = new Game("Team15");
+		assertEquals(0, driver.getBuildings());
+	}
+	
+	@Test
+	public void testGetWood(){ //Ensures getWood works correctly
+		Game driver = new Game("Team15");
+		assertEquals(0, driver.getWood());
+	}
+	
+	@Test
+	public void testGetStone(){ //Ensures getStone works correctly
+		Game driver = new Game("Team15");
+		assertEquals(0, driver.getStone());
+	}
+	
+	@Test
+	public void testUpdateStone(){ //Ensures updateStone works correctly
+		Game driver = new Game("Team15");
+		driver.updateStone(2);
+		assertEquals(2, driver.getStone());
+	}
+	
+	@Test
+	public void testUpdateWood(){ //Ensures updateWood works correctly
+		Game driver = new Game("Team15");
+		driver.updateWood(2);
+		assertEquals(2, driver.getWood());
+	}
+	
+	@Test
+	public void testUpdateFood(){ //Ensures updateFood works correctly
+		Game driver = new Game("Team15");
+		driver.updateFood(2);
+		assertEquals(47, driver.getFood());
+	}
+	
+	@Test
+	public void testGetDice(){ //Ensures a return from Dice class
+		Game driver = new Game("Team15");
+		assertNotNull(driver.getDice());
+	}
+
+}
